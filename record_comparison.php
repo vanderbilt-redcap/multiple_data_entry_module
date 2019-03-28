699275e5da27d605e6c048b51f32fa83fcb48859
Kyle McGuffin <kyle.mcguffin@vumc.org>
2019-03-28 04:34:00 -0400

    Add logic to pull matching fields from all records

--- record_comparison.php
+++ record_comparison.php
@@ -6,3 +6,32 @@ if($project == "") {
 	throw new Exception("No project selected");
 }
 
+require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
+
+/* @var $module RedcapAfrica\MultipleDataEntry\MultipleDataEntry */
+$matchingField = $module->getProjectSetting("record-name-field");
+$secondaryMatchingFields = $module->getProjectSetting("matching-fields");
+
+$recordDetails = $module->getData($project,"");
+
+foreach($recordDetails as $recordId => $details) {
+	$matchingRecords = [];
+	$matchingValue = "";
+	$secondaryValues = [];
+
+	foreach($details as $eventId => $eventDetails) {
+		if($eventDetails[$matchingField] != "") {
+			$matchingValue = $eventDetails[$matchingField];
+		}
+
+		foreach($secondaryMatchingFields as $secondaryField) {
+			if($eventDetails[$secondaryField] != "") {
+				$secondaryValues[$secondaryField] = $eventDetails[$secondaryField];
+			}
+		}
+	}
+
+	echo "Record: $matchingValue <Br />";var_dump($secondaryValues);echo "<Br /><br />";
+}
+
+require_once \ExternalModules\ExternalModules::getProjectFooterPath();
