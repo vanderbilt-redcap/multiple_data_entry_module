9ab90e5b10a43669e9a707bf19e3175dd5d46f93
Kyle McGuffin <kyle.mcguffin@vumc.org>
2019-03-28 05:57:26 -0400

    Add logic to pull data to be compared for all comparison records

--- record_comparison.php
+++ record_comparison.php
@@ -23,25 +23,47 @@ foreach($recordDetails as $recordId => $details) {
 	}
 
 	$matchingData = $module->findMatchingFields($details);
+//	echo "<bR />Initial Record: <Br />";
+//	var_dump($matchingData);
 
 	$thisMatchingRecords = [];
 
 	foreach($recordDetails as $thisId => $thisDetails) {
-		if($thisId == $recordId || array_key_exists($recordId,$matchedRecords)) {
+		if($thisId == $recordId || array_key_exists($thisId,$matchedRecords)) {
 			continue;
 		}
 
 		$thisMatchingData = $module->findMatchingFields($thisDetails);
-
-		if($matchedData[0] == $thisMatchingData[0]) {
+//		echo "<br />Secondary Records: <Br />";
+//		var_dump($thisMatchingData);
+		if($matchingData[0] == $thisMatchingData[0]) {
 			$thisMatchingRecords[] = $thisId;
 			$matchedRecords[$recordId] = 1;
 			$matchedRecords[$thisId] = 1;
 		}
 	}
-	$recordsToCompare[$recordId] = $matchedRecords;
+	$recordsToCompare[$recordId] = $thisMatchingRecords;
 }
 
-var_dump($recordsToCompare);
+$comparisonData = [];
+foreach($recordsToCompare as $initialRecord => $matchingRecords) {
+	$thisComparisonData = [];
+
+	$recordIds = array_merge([$initialRecord],$matchingRecords);
+
+	foreach($recordsToCompare as $recordId) {
+		foreach($recordDetails[$recordId] as $eventId => $eventDetails) {
+			foreach($eventDetails as $fieldName => $fieldValue) {
+				if(!array_key_exists($fieldName,$thisComparisonData)) {
+					$thisComparisonData[$fieldName] = [];
+				}
+				$thisComparisonData[$fieldName][$recordId] = $eventDetails[$fieldName];
+			}
+		}
+	}
+	$comparisonData[] = $thisComparisonData;
+}
+
+var_dump($comparisonData);
 
 require_once \ExternalModules\ExternalModules::getProjectFooterPath();
